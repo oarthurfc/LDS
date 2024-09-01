b26780d0c1c76e60207685e97e3cebb4a4e41905
oarthurfc <arthuryouface@gmail.com>
2024-09-01 15:23:21 -0300

    Removendo Lista de Turma da classe Professor

--- code/entities/Professor.java
+++ code/entities/Professor.java
@@ -1,50 +1,28 @@
 package code.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Professor implements Serializable {
    private String matricula;
     private String nome;
     private String CPF;
-    private List<Turma> turmas;
 
     public Professor(String matricula, String nome, String CPF) {
         this.matricula = matricula;
         this.nome = nome;
         this.CPF = CPF;
-        this.turmas = new ArrayList<>();
-    }
-
-    public List<Aluno> visualizarAlunos() {
-        List<Aluno> alunos = new ArrayList<>();
-        for (Turma turma : turmas) {
-            alunos.addAll(turma.getAlunos());
-        }
-        return alunos;
     }
 
     public String getNome() {
         return nome;
     }
 
-	public Object getMatricula() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'getMatricula'");
+	public String getMatricula() {
+		return matricula;
 	}
 
-    public List<Turma> getTurmas() {
-        return turmas;
-    }
-
-    public void adicionarTurma(Turma turma) {
-        turmas.add(turma);
-    }
-
-    public void add(Professor professor) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    public String getCPF() {
+        return CPF;
     }
 
 }
